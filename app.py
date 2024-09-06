2b7a7eedbb825e7a78e1e70f25ba08d189535338
Manju Vallabha Pavalla <143684911+Manju-Vallabha@users.noreply.github.com>
2024-09-06 22:42:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,46 @@
 import streamlit as st
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
-import contractions
 import re
-
-
+import joblib
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('punkt_tab')
-
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import SnowballStemmer
-import joblib
-import json
+import contractions
 
+# Download necessary NLTK data
+nltk.download('punkt')
+nltk.download('stopwords')
+
+# Set up Streamlit page configuration
 st.set_page_config(layout='wide')
 
-# Load your trained model and vectorizer
-model = joblib.load('svm_model.pkl')  # Load the SVM model from the file
-
-# Load your TF-IDF vectorizer here
-vectorizer = joblib.load('tfidf_vectorizer.pkl')  # Load the TF-IDF vectorizer from the file
+# Load trained model and vectorizer
+model = joblib.load('svm_model.pkl')
+vectorizer = joblib.load('tfidf_vectorizer.pkl')
 
 # Streamlit app title
 st.title('Amazon Reviews Sentiment Analysis')
-# Text input for user to enter a review
-# Add a centering column
+
+# Create a centered column for text input
 col1, col2, col3 = st.columns([1, 6, 1])
 with col2:
     user_input = st.text_area('Enter a review:')
 
     if st.button('Analyze Sentiment'):
         if user_input:
-        # Preprocess the input text
-            user_input = re.sub(r'[\W_]+', ' ', contractions.fix(re.sub(r'\d+', '', user_input.replace(' s ', ' ')))).lower()
-            tokens = nltk.word_tokenize(user_input)
-            user_input = " ".join([token for token in tokens if token not in stopwords.words('english')])
-            user_input = ' '.join([SnowballStemmer("english").stem(word) for word in user_input.split()])
+            # Preprocess the input text
+            user_input = re.sub(r'[\W_]+', ' ', contractions.fix(re.sub(r'\d+', '', user_input))).lower()
+            tokens = word_tokenize(user_input)
+            user_input = ' '.join([SnowballStemmer('english').stem(token) for token in tokens if token not in stopwords.words('english')])
 
-        # Vectorize the input text using the loaded TF-IDF vectorizer
+            # Vectorize the input text
             user_input_vec = vectorizer.transform([user_input])
 
-        # Make a prediction
+            # Make a prediction
             prediction = model.predict(user_input_vec)
 
-        # Display the result
-            if prediction == 0:
-                st.info('Sentiment: Negative')
-    
-            else:
-                st.info('Sentiment: Positive')
+            # Display the result
+            sentiment = 'Positive' if prediction == 1 else 'Negative'
+            st.info(f'Sentiment: {sentiment}')
         else:
             st.write('Please enter a review for analysis.')
